731cdbdb0345c0c72590f680e0a49fe80b964842
Mandha <sukmanmandha63@gmail.com>
2021-07-18 15:25:14 +0700

    SIG_Mandha_Update

---
+++ webgis/data/smakabupaten_5.js
@@ -0,0 +1,16 @@
+var json_smakabupaten_5 = {
+"type": "FeatureCollection",
+"name": "smakabupaten_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "SMA": "SMAN 1 KAJEN" }, "geometry": { "type": "Point", "coordinates": [ 109.586275562213459, -7.027518644568383 ] } },
+{ "type": "Feature", "properties": { "SMA": "SMAN1 KESESI" }, "geometry": { "type": "Point", "coordinates": [ 109.513397319660655, -7.012898009488338 ] } },
+{ "type": "Feature", "properties": { "SMA": "SMAN1 SRAGI" }, "geometry": { "type": "Point", "coordinates": [ 109.560656287647348, -6.936939917178774 ] } },
+{ "type": "Feature", "properties": { "SMA": "SMAN 1 WIRADESA" }, "geometry": { "type": "Point", "coordinates": [ 109.624335353493208, -6.889190710934969 ] } },
+{ "type": "Feature", "properties": { "SMA": "SMAN 1 KEDUNGWUNI" }, "geometry": { "type": "Point", "coordinates": [ 109.641551367581599, -6.960090697932805 ] } },
+{ "type": "Feature", "properties": { "SMA": "SMAN 1 KANDANGSERANG" }, "geometry": { "type": "Point", "coordinates": [ 109.520179910135766, -7.121523272249036 ] } },
+{ "type": "Feature", "properties": { "SMA": "SMAN 1 PANINGGARAN" }, "geometry": { "type": "Point", "coordinates": [ 109.589476529657418, -7.169679087870808 ] } },
+{ "type": "Feature", "properties": { "SMA": "SMAN 1 DORO" }, "geometry": { "type": "Point", "coordinates": [ 109.673693694722317, -7.030336072669664 ] } },
+{ "type": "Feature", "properties": { "SMA": "SMAN 1 BOJONG" }, "geometry": { "type": "Point", "coordinates": [ 109.603425249949865, -6.979780973342449 ] } }
+]
+}
